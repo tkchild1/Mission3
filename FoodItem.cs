604e277fa6d91b4d2141dd6b44f0496037eb31e4
tkchild1 <tkchild1@byu.edu>
2026-01-20 13:25:24 -0700

    Add files via upload

---
+++ FoodItem.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace Mission3
+{
+    internal class FoodItem
+    {
+        string name;
+        string category;
+        int quantity;
+        string expirationDate;
+        public FoodItem(string name, string category, int quantity, string expirationDate)
+        {
+            this.name = name;
+            this.category = category;
+            this.quantity = quantity;
+            this.expirationDate = expirationDate;
+        }
+
+        public static FoodItem addItem()
+        {
+            Console.WriteLine("Please enter your item: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your item category (e.g. Dairy, Produce, Canned Goods): ");
+            string category = Console.ReadLine();
+            Console.WriteLine("Please enter your item quantity: ");
+            int quantity;
+            while (!int.TryParse(Console.ReadLine(), out quantity) || quantity <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer: ");
+            }
+            Console.WriteLine("Please enter your items expiration date: ");
+            string expirationDate = Console.ReadLine();
+
+            return new FoodItem(name, category, quantity, expirationDate);
+
+        }
+        public void PrintItem()
+        {
+            Console.WriteLine($"Name: {name}, Category: {category}, Quantity: {quantity}, Expiration: {expirationDate}");
+        }
+
+        public static void PrintAllItems(List<FoodItem> items)
+        {
+            Console.WriteLine("\nCurrent Food Inventory");
+            if (items.Count == 0)
+            {
+                Console.WriteLine("No items in inventory.\n");
+            }
+            else
+            {
+                for (int i = 0; i < items.Count; i++)
+                {
+                    Console.Write($"{i + 1}. ");
+                    items[i].PrintItem();
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void RemoveItem(List<FoodItem> items)
+        {
+            Console.WriteLine("Enter the name of the food item you wish to remove.\n");
+            string deleteItem = Console.ReadLine();
+            bool found = false;
+            for (int i =0; i < items.Count; i++)
+            {
+                if(deleteItem == items[i].name)
+                {
+                    items.Remove(items[i]);
+                    found = true;
+                    Console.WriteLine($"{deleteItem} was successfully removed.\n");
+                    break;
+                }
+                
+            }
+            if (!found)
+            {
+                Console.WriteLine($"{deleteItem} was not found in inventory.\n");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Tanner Child
+// Mission 3 Food Bank Assignment
+
+using Mission3;
+
+Console.WriteLine("Welcome to your Food Bank");
+
+bool keep = true;
+List<FoodItem> foodInventory = new List<FoodItem>();
+
+while (keep)
+{
+    Console.WriteLine("Here is the menu:\n\n1. Add Food Item\n2. Delete Food Item\n3. Print List of Current Food Items\n4. Exit the Program\n");
+    string userResponse = Console.ReadLine();
+
+    if (userResponse == "1")
+    {
+        FoodItem newItem = FoodItem.addItem(); // Call a static method to create item
+        foodInventory.Add(newItem);
+        Console.WriteLine("Item added successfully!\n");
+    }
+    else if (userResponse == "2")
+    {
+        FoodItem.RemoveItem(foodInventory);
+    }
+    else if (userResponse == "3")
+    {
+        FoodItem.PrintAllItems(foodInventory);
+    }
+    else if (userResponse == "4")
+    {
+        keep = false;
+    }
+    else
+    {
+        Console.WriteLine("Please enter a valid number.\n");
+    }
+}
+
+Console.WriteLine("Thank you! Have a nice day");
+
+
