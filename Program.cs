38dbc4ba26a799ec66ec5ee4a4c1cd6c83dad689
tkchild1 <tkchild1@byu.edu>
2026-01-21 15:07:41 -0700

    Add comments
    Added comments for clarity and organization.

--- Program.cs
+++ Program.cs
@@ -3,16 +3,20 @@
 
 using Mission3;
 
+//Welcome Message
 Console.WriteLine("Welcome to your Food Bank");
 
+// Create an array of foodItems and a boolean to keep the loop going
 bool keep = true;
 List<FoodItem> foodInventory = new List<FoodItem>();
 
+// While loop that displays the menu until the user quits, and shows each option.
 while (keep)
 {
     Console.WriteLine("Here is the menu:\n\n1. Add Food Item\n2. Delete Food Item\n3. Print List of Current Food Items\n4. Exit the Program\n");
     string userResponse = Console.ReadLine();
 
+    // If statements that call the method that goes along with the user input, for all 4 options
     if (userResponse == "1")
     {
         FoodItem newItem = FoodItem.addItem(); // Call a static method to create item
@@ -33,10 +37,12 @@ while (keep)
     }
     else
     {
+        // Error handling if they enter an invalid number
         Console.WriteLine("Please enter a valid number.\n");
     }
 }
 
 Console.WriteLine("Thank you! Have a nice day");
+
 
 
